6ad8bd8559812efdafd8bd96e333d79d54d4421c
Rodrigo Cardoso <a14628@oficina.pt>
2024-10-24 14:56:41 +0100

    Create Jogo.py

---
+++ Jogo.py
@@ -0,0 +1,33 @@
+print("Bem-Vindo á Floresta com um tesouro!!")
+print("Estas em uma floresta e precisas de encontrar o tesouro perdido.")
+print("A tua missão é encontrar o tesouro perdido e voltar para casa.")
+print("Boa sorte!!")
+
+print("Encontraste uma gruta, a tua escolha é: ")
+escolha = input("Tens dois caminhos o da Entrar/Fugir:")
+if escolha == "Entrar":
+  print("Encontraste uma alcateia de lobos, a tua escolha é: " )
+  input("Tens de escolher entre fugir/lutar:")
+else:
+  print("Foste ferido na perna Esquerda por um Lobo!!")
+if escolha == "Curar/Continuar":
+  print("Morreste, Com uma emorrgia na perna esquerda")
+  if escolha == "Fugir":
+      print("Morreste por um Crocodilo ao voltar, COBARDE, FIM DE JOGO")
+  
+
+
+
+
+
+
+
+
+
+
+
+
+#Print(“Bem-Vindo á Floresta Encantada")
+#Print(“Houve um homem que entrou numa Floresta Encantada”)
+#Print(“Nessa Floresta havia um tesouro dentro de uma gruta”)
+#Escolha = input(“Escolha de o Homem deve ir á procura do tesouro/Fugir e ir pra sua casa”)
